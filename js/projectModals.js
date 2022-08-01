84a8c7d563998f15024f98c506145610e0b1c2d7
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-08-01 15:44:08 -0300

    feat: data changed and styles removed

--- js/projectModals.js
+++ js/projectModals.js
@@ -33,6 +33,36 @@ const objSites = [
         name: 'TeamSoft',
         image: 'images/portfolio/teamSoft.png',
         linkHref: 'https://team-soft.netlify.app/'
+    },
+    {
+        name: 'Agência de Viagens',
+        image: 'images/portfolio/agencia.png',
+        linkHref: 'https://rafael-doctom.github.io/agencia-de-viagens/'
+    },
+    {
+        name: 'ToRecruiter',
+        image: 'images/portfolio/toRecruiter.png',
+        linkHref: 'https://rafael-doctom.github.io/toRecruiter/'
+    },
+    {
+        name: 'Crachá NLW',
+        image: 'images/portfolio/nlw.png',
+        linkHref: 'https://rafael-doctom.github.io/nlw_heat_challenge-01/'
+    },
+    {
+        name: 'Text Generator',
+        image: 'images/portfolio/text-generator.png',
+        linkHref: 'https://rafael-doctom.github.io/text_generator/'
+    },
+    {
+        name: 'Fone Landing',
+        image: 'images/portfolio/fone-landing.png',
+        linkHref: 'https://rafael-doctom.github.io/fone-landing/'
+    },
+    {
+        name: 'Harry Potter',
+        image: 'images/portfolio/harry-potter.png',
+        linkHref: 'https://harry-potter-api0.netlify.app/'
     }
 ];
 
@@ -41,7 +71,6 @@ const objAplicativos = [
         name: 'IMC',
         image: 'images/portfolio/imc.png',
         linkHref: 'https://github.com/Rafael-doctom/imc'
-
     }
 ];
 
@@ -50,46 +79,64 @@ let mapSites = objSites.map((item) => {
     return `
     <div class="card1">
         <img class="imageCard" src=${item.image} alt="sei la"/>
-        <a class="hrefCard" href=${item.linkHref}>${item.name}</a>
+        <a class="hrefCard" target="_blank" href=${item.linkHref}>${item.name}</a>
     </div>`
 });
 
 let mapAplicativos = objAplicativos.map((item) => {
     return `
-        <div class="card1">
+        <div class="card2">
             <img class="imageCard" src=${item.image} />
-            <a class="hrefCard" href=${item.linkHref}>${item.name}</a>
+            <a class="hrefCard" target="_blank" href=${item.linkHref}>${item.name}</a>
         </div>`
 });
 
 
 const handleClickAll = () => {
     allProjects.style.display = 'flex'
-
-    if (portfolioSites.style.display = 'none') {
-        portfolioSites.style.display = 'none'
-    }
-
-    if (aplicativos.style.display = 'none') {
+    portfolioSites.style.display = 'none'
         aplicativos.style.display = 'none'
-    }
+
+    // if (allProjects.style.display = 'none' && portfolioSites.style.display == 'none' && aplicativos.style.display == 'none') {
+    //     portfolioSites.style.display = 'none'
+    //     aplicativos.style.display = 'none'
+    //     allProjects.style.display = 'none'
+    // }
+
+
+
+    // alert('aaa')
 };
 
 const handleClickSite = () => {
-    if (portfolioSites.style.display = 'none') {
-        allProjects.style.display = 'none'
-        portfolioSites.style.display = 'flex'
-    }
+
+    portfolioSites.style.display = 'flex'
+    aplicativos.style.display = 'none'
+    allProjects.style.display = 'none'
+
+
+    // if (portfolioSites.style.display = 'none') {
+    //     allProjects.style.display = 'none'
+    //     aplicativos.style.display = 'none'
+    //     portfolioSites.style.display = 'flex'
+
+    // }
 
     document.querySelector("#siteId").innerHTML = mapSites.join("");
 };
 
 const handleClickApp = () => {
-    if (aplicativos.style.display = 'none') {
-        allProjects.style.display = 'none'
-        portfolioSites.style.display = 'none'
-        aplicativos.style.display = 'flex'
-    }
+
+    portfolioSites.style.display = 'none'
+    aplicativos.style.display = 'flex'
+    allProjects.style.display = 'none'
+
+
+    // if (aplicativos.style.display == 'none') {
+    //     allProjects.style.display == 'none'
+    //     portfolioSites.style.display == 'none'
+    //     aplicativos.style.display == 'flex'
+    // }
 
     document.querySelector("#appID").innerHTML = mapAplicativos.join("");
 };
